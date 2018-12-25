414b08c64aed38924789720f5bd2f99e7afe2ef7
yuangezhizao <root@yuangezhizao.cn>
2018-12-25 21:04:12 +0800

    0.0.0.0

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from main import app
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, threaded=True)
+    app.debug = True
+    app.run(host='0.0.0.0', port=2233, threaded=True)
