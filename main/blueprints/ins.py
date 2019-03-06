39ff1e7890fcc0390adb5254c6d832ed9acabc41
yuangezhizao <root@yuangezhizao.cn>
2019-03-06 20:38:18 +0800

    图片列表页；用户页“设置公开权限”按钮开放

--- main/blueprints/ins.py
+++ main/blueprints/ins.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, redirect, url_for
+from flask import Blueprint, flash, redirect, url_for, render_template, request
 from flask_login import login_required
 
 from main.models.photo import Photo
@@ -13,16 +13,21 @@ ins_bp = Blueprint('ins', __name__)
 @permission_required('SET_PUBLIC')
 def set_public(photo_id):
     photo = Photo.query.get_or_404(photo_id)
-    if photo.can_public:
-        photo.can_public = False
-        flash('已关闭公开权限', 'info')
-    else:
-        photo.can_public = True
-        flash('已开启公开权限', 'info')
+    status = request.form.get('status')
+    photo.status = status
+    flash(f'状态码已设置为：{status}', 'info')
     db.session.commit()
     return redirect(url_for('user.show_photo', photo_id=photo_id))
 
+
 @ins_bp.route('/photos_list')
+@login_required
+@permission_required('SET_PUBLIC')
 def photos_list():
-    # 笔记本没电了，为使单元测试通过，使用手机提交，暂时 pass 处理
-    pass
+    all_count = Photo.query.count()
+    wait_count = Photo.query.filter_by(status=0).count()
+    passed_count = Photo.query.filter_by(status=1).count()
+    not_passed_count = Photo.query.filter_by(status=-1).count()
+    photos = Photo.query.all()
+    return render_template('ins/photos_list.html', wait_count=wait_count, all_count=all_count,
+                           passed_count=passed_count, not_passed_count=not_passed_count, photos=photos)
