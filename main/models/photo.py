fe093bf3a3014ba570050f1728b9e043912e8921
yuangezhizao <root@yuangezhizao.cn>
2019-04-04 14:30:55 +0800

    revoke（玄学 bug 解决

--- main/models/photo.py
+++ main/models/photo.py
@@ -68,7 +68,6 @@ class Advive(db.Model):
     passed_count = db.Column(db.Integer)
     url = db.Column(db.Text)
     filter = db.Column(db.Text)
-    body = db.Column(db.Text)
     status = db.Column(db.Integer, default=0)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
 
